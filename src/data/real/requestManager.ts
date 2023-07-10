3ede842bed870a366e62438d498888608a104c45
juampi senmartin <juanpablo.senmartin@ing.austral.edu.ar>
2023-07-10 00:47:41 -0300

    update version

--- src/data/real/requestManager.ts
+++ src/data/real/requestManager.ts
@@ -49,7 +49,7 @@ export class RequestManager {
                 content:btoa(snippet.content),
                 snippetName:snippet.name,
                 type:snippet.type.toUpperCase(),
-                version:1.0
+                version:1.1
             },
             () => {
                 console.log("snippet created")
